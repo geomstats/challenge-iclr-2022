be6a7a45961b91f0092ad3d8c32c57c4e897f72d
Rishi Sonthalia <rishisonthalia94@gmail.com>
2022-03-18 17:24:57 -0700

    Update documentation

--- rsonthal_xfcui/TreeRepresentation.py
+++ rsonthal_xfcui/TreeRepresentation.py
@@ -4,7 +4,8 @@ import torch
 import numpy as np
 
 class TreeRep():
-  """Class for running the TreeRep Algorithm.
+  """
+  Class for running the TreeRep Algorithm.
 
   The algorithm takes in an n by n symmetric matrix
   with positive entries that should represent a metric.
@@ -42,10 +43,11 @@ class TreeRep():
     self.debug = False
 
 
-  """
+  def gid(self,D,w,x,y):
+    """
     Inputs
     ---------
-    D : n x n torch.Tensor 
+    D : n x n numpy.ndarray or torch.Tensor
       That represents the matrix for the metric
     
     w,x,y : ints
@@ -58,11 +60,22 @@ class TreeRep():
     --------
       Returns a float that is the gromov product
       of x with y with respect to base w
-  """
-  def gid(self,D,w,x,y):
+    """
     return 0.5*(D[w,x]+D[w,y]-D[x,y])
   
+  
   def extract_metric(self):
+    """
+    Inputs
+    ---------
+    no inputs
+    
+    Outputs
+    --------
+      Returns a numpy.ndarry or torch.Tensor that n x n
+      that represents the metric given by the all pairs
+      shortest path metrics on the learned tree. 
+    """
     d = nx.algorithms.shortest_paths.dense.floyd_warshall(self.G)
     self.D = gs.zeros((self.n,self.n))
     for i in range(self.n):
@@ -71,7 +84,9 @@ class TreeRep():
 
     return self.D
 
-  """
+
+  def contract_ra(self, r, a, b, c, V):
+    """
     Inputs
     --------
       r : int
@@ -97,7 +112,7 @@ class TreeRep():
       False if the edge is not contracted and
       gived the old center node r
     """
-  def contract_ra(self, r, a, b, c, V):
+    
     if gs.abs(self.W[r,a]) < self.tol:
       for v in V:
         self.W[r,v] = 0
@@ -121,7 +136,40 @@ class TreeRep():
       return True, a
     return False, r
 
+
   def sort_into_zones(self, V, r, x, y, z, replaced_root = False):
+    """
+    Inputs
+    ---------
+    
+    V : list
+      Contains the nodes that have yet to be sorted
+    r : int
+      Steiner node of the universal tree
+    x,y,z : int
+      Other three nodes of the universal tree. That, is 
+      x - r - y
+          |
+          z
+      is the tree. 
+    replaced_root : bool
+      This stores whether an edge in the above tree has been 
+      contracted already. 
+  
+    Outputs
+    ----------
+    Returns a 7 elements list of tuples of the form (L,zt,a,b)
+    where L is a list of the nodes sorted into that zone
+    zt is the zone type
+    a is the nodes primarily associated with that zones. For
+    zones of type 1, b = a
+    b is the node secondarily associated with that zone. For 
+    zones of type 2 b is not the same as a
+    
+    
+    This is based on definiton 10 in https://arxiv.org/pdf/2005.03847.pdf
+      
+    """
     n = self.n
     X1 = []
     X2 = []
@@ -188,8 +236,32 @@ class TreeRep():
     Zones = [(R1,1,r,r),(X1,1,x,x),(X2,2,x,r),(Y1,1,y,y),(Y2,2,y,r),(Z1,1,z,z),(Z2,2,z,r)]
 
     return Zones
+  
 
   def add_steiner_node(self,x,y,z):
+    """
+    Inputs
+    ---------
+    
+    x,y,z : int
+      Points in the metric space
+    
+    
+    Outputs
+    ---------
+    
+    Adds the appropriate steiner node r to create the graph
+    x - r - y
+        |
+        z
+    Here the edge lengths are set so that that they respect
+    the original metric. This method will contract an edge 
+    if the edge length is 0. 
+    
+    This is based on Lemma 1 and definiton 9 in https://arxiv.org/pdf/2005.03847.pdf
+  
+    """
+    
     r = self.nextroots.pop(-1)
 
     #check if we need to make W bigger
@@ -228,7 +300,33 @@ class TreeRep():
 
     return replaced_root, r
 
+  
+
   def zone1_helper(self,V,x):
+    """
+    Inputs
+    ---------
+    
+    x: int
+      Primary node associated with the zone
+    V : list
+      Nodes in the zone
+    
+    
+    Outputs
+    ---------
+    Does the recurvesive step of the algorithm. 
+    
+    Specifically, if V is empty of has 1 node it returns
+    after connecting that one node to z. 
+    
+    If the length of V is bigger than 1. It picks 2 others
+    nodes y,z from V at random and constructs universal tree
+    for x,y,z. 
+    
+    It then sorts the rest of V into the new zones
+    """
+  
     if len(V) == 0:
       return []
     
@@ -252,8 +350,34 @@ class TreeRep():
     replaced_root, r = self.add_steiner_node(x,y,z)
     Zones = self.sort_into_zones(V_rem,r,x,y,z,replaced_root)
     return Zones
-
+  
   def zone2_helper(self,V,x,y):
+    """
+    Inputs
+    ---------
+    
+    x : int
+      Primary node associated with the zone
+    y : int
+      Secondary node associated with the zone
+    V : list
+      Nodes in the zone
+    
+    
+    Outputs
+    ---------
+    Does the recurvesive step of the algorithm. 
+    
+    Specifically, if V is empty of has 1 node it returns
+    after connecting that one node to z. 
+    
+    If the length of V is bigger than 1. It picks 2 others
+    nodes y,z from V at random and constructs universal tree
+    for x,y,z. 
+    
+    It then sorts the rest of V into the new zones 
+  
+    """
     if len(V) == 0:
       return []
 
@@ -273,8 +397,25 @@ class TreeRep():
     
     Zones = self.sort_into_zones(V_rem,r,x,y,z,replaced_root)
     return Zones
+  
 
   def learn_tree(self):
+    """
+    Inputs
+    ---------
+    
+    None
+    
+    
+    Outputs
+    ---------
+    Main mathod that runs the recursive TreeRep Algorithm
+    
+    self.G has been updated to store the learned
+    tree. 
+  
+    """
+    
     # Create the weight matrix for the output tree. 
     # It is bigger than n x n since the algorith
     # adds steiner nodes. 
@@ -291,10 +432,13 @@ class TreeRep():
     for i in range(3,len(p)):
       V.append(p[i].item())
 
+    # Create the universal tree
     replaced_root, r = self.add_steiner_node(x,y,z)
 
+    # sort into zones
     Zones = self.sort_into_zones(V,r,x,y,z,replaced_root)
 
+    # Do the recursion
     while(len(Zones)>0):
       V,zt,a,b = Zones.pop(0)
       if zt == 1:
@@ -304,6 +448,8 @@ class TreeRep():
         new_zones = self.zone2_helper(V,a,b)
         Zones.extend(new_zones)
 
+    # Some edge weights may become negative. Set these edge weights to 
+    # a small number. 
     for e in self.G.edges():
       self.G[e[0]][e[1]]['weight'] = np.maximum(1e-5,self.W[e[0],e[1]])
     
