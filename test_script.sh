8f175b88f1606a9dfc2b5d019dd42bbe35f01120
Saiteja Utpala <saitejautpala@gmail.com>
2022-03-18 22:20:50 -0700

    fix everything

--- test_script.sh
+++ test_script.sh
@@ -1,6 +1,6 @@
 for n in */*.ipynb
 do
-    if [[ "$n" == "submission-example-1/clustering_using_riemannian_mean_shift_algorithm.ipynb" ]]
+    if [[ "$n" != "submission-example-1/clustering_using_riemannian_mean_shift_algorithm.ipynb" ]]
     then
             poetry run jupyter nbconvert --to notebook --execute $n
     else
