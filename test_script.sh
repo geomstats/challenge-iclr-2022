659137904413cfb169144debe6583b1da23837ae
Saiteja Utpala <saitejautpala@gmail.com>
2022-04-04 06:45:57 -0700

    remove typo

--- test_script.sh
+++
@@ -1,9 +0,0 @@
-for n in */*.ipynb
-do
-    if [[ "$n" != "submission-example-1/clustering_using_riemannian_mean_shift_algorithm.ipynb" ]]
-    then
-            poetry run jupyter nbconvert --to notebook --execute $n
-    else
-            echo "No notebooks found!"
-    fi
-done
